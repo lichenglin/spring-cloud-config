9a904c7cdc36758fb0b14efccdeef6df43213581
lichenglin <lichenglin0616@126.com>
2018-03-24 21:23:27 +0800

    123

---
+++ config-server/src/main/java/com/licl/spring/cloud/config/ConfigServer.java
@@ -0,0 +1,13 @@
+package com.licl.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+}
